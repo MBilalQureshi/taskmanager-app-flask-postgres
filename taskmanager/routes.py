841f5652ae4490f21c02ca6aa377362264f35124
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-09-27 20:36:19 +0000

    Import tables schema in rotes, create db taskmager and generate tables(both via terminal)

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from taskmanager import app, db
-
+# At the top of the routes, we need to import these classes in order to generate our database next.
+from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
