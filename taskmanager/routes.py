1f42249f9ed451e9902e29cab75da2510938c0c9
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-09-29 16:27:35 +0000

    Add materialize css and js links also add custom files links as well

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -3,6 +3,7 @@ from taskmanager import app, db
 # At the top of the routes, we need to import these classes in order to generate our database next.
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("base.html")
