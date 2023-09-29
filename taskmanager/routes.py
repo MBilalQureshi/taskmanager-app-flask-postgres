dbca14ea981dd0bfe94fd69f5409773883ad62fc
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-09-29 18:34:31 +0000

    Add new category html files and route them

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,9 +1,22 @@
 from flask import render_template
 from taskmanager import app, db
-# At the top of the routes, we need to import these classes in order to generate our database next.
+# At the top of the routes, we need to import these classes in order to
+# generate our database next.
 from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+# we need to include a list of the two methods "GET" and "POST", since we will
+# be submitting a form to the database.
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
