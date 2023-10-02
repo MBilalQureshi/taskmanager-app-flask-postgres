c53290a00cb453f26ef5d3604335670486325da3
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-10-02 21:12:25 +0000

    Add the add new tasks functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  //sidenav initialization  
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // Modals initialization
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  });
+  // select initialization
+  let select = document.querySelectorAll('select');
+  M.FormSelect.init(select);
+});
